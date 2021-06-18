1a83fbab240dfd0f68ebb78623719662467302f7
A248 <theanandbeh@gmail.com>
2021-06-18 11:53:32 -0400

    Export package in module descriptor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module net.kyori.adventure.text.serializer.legacytext3 {
+    exports net.kyori.adventure.text.serializer.legacytext3;
 
     requires transitive net.kyori.adventure;
     requires transitive net.kyori.text;
